fc40274f4680bb3d5c0683dcd4ffe29fbe5f7f79
mars <kooptsov@gmail.com>
2020-01-21 15:46:25 +0300

    smpb

--- parsing/parsing_2.py
+++ parsing/parsing_2.py
@@ -64,7 +64,7 @@ for i in ss['results']:
             y = x[0].replace('dim','')
             m = periods[x[1]]
             query = "INSERT INTO price.tab(region, products, ymd, price, type) " \
-                    "VALUES(%s,%s,%s,%s)"
+                    "VALUES(%s,%s,%s,%s,%s)"
             args = (reg, prod, datetime.datetime(int(y),int(m),1).strftime('%Y-%m-%d %H:%M:%S'), float(v.replace(',','.')), 'rosstat')
             cursor = connection.cursor()
             cursor.execute(query, args)
